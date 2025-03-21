ccdadb0a75f60b3adc6a4c714a81749dcb682f53
AtharvaNarkhede1 <144204822+AtharvaNarkhede1@users.noreply.github.com>
2025-03-21 09:59:27 +0530

    update

---
+++ dijkstra.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+vector<int> dijkstra(vector<vector<int>> &vec, int vertices, int egdes, int source){
+    //create adjacency list
+    unordered_map<int, list <pair<int, int>>> adj;
+    for(int i=0; i<vertices; i++){
+        int u = vec[i][0];
+        int v = vec[i][1];
+        int w = vec[i][2];
+
+        adj[u].push_back
+        
+    }
+}
